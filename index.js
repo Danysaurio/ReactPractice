d7418ff4d4fafaac9fb08465add6b494f0fc8f30
Daniel Zavala <Dany@Daniels-iMac.lan>
2018-03-14 23:24:52 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const container = document.getElementById('app');
+const holaMundo = <h1> soy danisaurio</h1>;
+ReactDOM.render(holaMundo, app);
